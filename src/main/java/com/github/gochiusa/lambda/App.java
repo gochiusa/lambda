25156a5d06b2ee5fe3b58dfdc071107217748bd8
gomi_ningen <korun24276@gmail.com>
2016-02-27 21:54:42 +0900

    簡単なセリフを返すlambda functionを作成🐰

---
+++ src/main/java/com/github/gochiusa/lambda/App.java
@@ -0,0 +1,31 @@
+package com.github.gochiusa.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class App {
+
+    private static final Random random = new Random();
+    private static final List<String> words = Arrays.asList(
+            "喫茶店... ラビットハウス...",
+            "この前お客さんにココアちゃんはシスター・コンプレックスだねって言われちゃった",
+            "リゼちゃん聞いてー私シスター・コンプレックスなんだって",
+            "今回の新作もすごいねー まるで本物の戦場だよ",
+            "うっさぎ〜 うっさぎ〜♪",
+            "ウサギがいない!?",
+            "コーヒー3杯頼んだから3回触る権利を手に入れたよ!",
+            "この上品な香り！これがブルーマウンテンかー",
+            "この酸味・・・キリマンジャロだね",
+            "安心する味！これインスタントの・・・",
+            "はぁ〜もふもふ気持ちいい〜 いけないよだれが・・・",
+            "なんかこの子にダンディな声で拒絶されたんだけど"
+    );
+
+    public String handler(final Context context) {
+        return words.get(random.nextInt(words.size()));
+    }
+
+}
